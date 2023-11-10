44102cd79c8c5cd24b4e679fbbb1c8792ba51877
syifanathaniar <140411045+syifanathaniar@users.noreply.github.com>
2023-11-10 22:08:50 +0700

    update navbar & trending

--- assets/Javascript/index.js
+++ assets/Javascript/index.js
@@ -1,3 +1,18 @@
+// Javascript Hamburger START
+const header = document.querySelector("header");
+const hamburgerBtn = document.querySelector("#hamburger-btn");
+const closeMenuBtn = document.querySelector("#close-menu-btn");
+
+// Toggle mobile menu on hamburger button click
+hamburgerBtn.addEventListener("click", () => header.classList.toggle("show-mobile-menu"));
+
+// Close mobile menu on close button click
+closeMenuBtn.addEventListener("click", () => hamburgerBtn.click());
+
+// Javascript Hamburger END
+
+
+
 let slideIndex = 0;
 showSlides();
 
@@ -17,3 +32,22 @@ function showSlides() {
   dots[slideIndex-1].className += " active";
   setTimeout(showSlides, 5000); // Change image every 5 seconds
 }
+
+// trending news start
+const initSlider = () => {
+  const imageList = document.querySelector(".slider-wrapper .mySlide-list");
+  const slideButtons = document.querySelectorAll(".slider-wrapper .slide-button");
+
+// Slide images according to the slide button clicks
+  slideButtons.forEach(button => {
+      button.addEventListener("click", () => {
+          const direction = button.id === "prev-slide" ? -1 : 1;
+          const scrollAmount = imageList.clientWidth * direction;
+          imageList.scrollBy({ left: scrollAmount, behavior: "smooth"});
+      });
+  });
+}
+
+window.addEventListener("load", initSlider);
+
+// trending new end
